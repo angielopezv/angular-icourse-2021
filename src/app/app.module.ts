dc7bc37e1595f5302749ce13827df98018eb39a3
Angie0310 <angie.lopez@agrosoft.com.ec>
2021-11-24 15:01:51 -0500

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
+ 
+
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, Test1Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
